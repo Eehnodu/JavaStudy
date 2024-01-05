96f775b93437ed83eb6e9068d7740b684c797dca
WooDongHee <WooDongHee@gmail.com>
2024-01-05 14:10:50 +0900

    3

--- JavaFestival/src/day4/Ex03.java
+++ JavaFestival/src/day4/Ex03.java
@@ -6,15 +6,25 @@ public class Ex03 {
 
 	public static void main(String[] args) {
 		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("행 개수 : ");
+//		int num = sc.nextInt();
+//		for(int i = num; i > 0; i--) {
+//			for(int j = 1; j < i; j++) {
+//				System.out.print(" ");
+//			}
+//			for(int k = 0; k <= num-i; k++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.print("행 개수 : ");
 		int num = sc.nextInt();
 		for(int i = num; i > 0; i--) {
-			for(int j = 1; j < i; j++) {
-				System.out.print(" ");
-			}
-			for(int k = 0; k <= num-i; k++) {
-				System.out.print("*");
+			for(int j = num; j > 0; j--) {
+				System.out.print(j <= num-i+1 ? "*": " ");
 			}
 			System.out.println();
 		}
--- Programmers/src/Programmers/Main.java
+++ Programmers/src/Programmers/Main.java
@@ -1,11 +1,31 @@
 package Programmers;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		System.out.println(a-b);
-	}
+    public static void main(String args[]) {
+        // 첫 번째 방식: 배열 직접 생성 후 출력
+        long startTime1 = System.currentTimeMillis();
+        int start = 3, end = 10;
+        int[] arr = new int[end - start + 1];
+        for (int i = start, j = 0; i <= end; i++, j++) {
+            arr[j] = i;
+        }
+        System.out.println(Arrays.toString(arr));
+        long endTime1 = System.currentTimeMillis();
+        System.out.println("첫 번째 방식 소요 시간: " + (endTime1 - startTime1) + "ms");
+
+        // 두 번째 방식: List를 이용하여 생성 후 출력
+        long startTime2 = System.currentTimeMillis();
+        List<Integer> list = new ArrayList<>();
+        for (int i = start; i <= end; i++) {
+            list.add(i);
+        }
+        int[] arr2 = list.stream().mapToInt(Integer::intValue).toArray();
+        System.out.println(Arrays.toString(arr2));
+        long endTime2 = System.currentTimeMillis();
+        System.out.println("두 번째 방식 소요 시간: " + (endTime2 - startTime2) + "ms");
+    }
 }
