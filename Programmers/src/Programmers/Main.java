6ab31b8c27bd574f346735db2aef22c5d8c4907c
WooDongHee <WooDongHee@gmail.com>
2024-01-09 15:54:31 +0900

    5

--- Programmers/src/Programmers/Main.java
+++ Programmers/src/Programmers/Main.java
@@ -6,50 +6,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-    	int a=6, b=4, c=2, d=5;
-    	int[] num = {a,b,c,d};
-    	int answer = 0;
-    	int min = num[0];
-    	for(int i = 0; i< num.length; i++) {
-    		min = min > num[i]? num[i]: min;
-    	}
-    	if(a == b) {
-    		if(b == c) {
-    			answer = c==d? a * 1111: (10 * a + d) * (10 * a + d);
-    			
-    		}else {
-    			if(c==d && a==d) {
-    				answer = (10 * a + b) * (10 * a + b);
-    			}else if (c==d && a!=d){
-    				answer = (a+c) * Math.abs(a-c);
-    			}else if(a==c && c!=d){
-    				answer = b*d;
-    			}else {
-    				answer = c*d;
-    			}
-    		}
-    	}else {
-    		if(b==c) {
-    			if(c==d && a!=d){
-    				answer = (10 * b + a) * (10 * b + a);
-    			}else if(c!=d && a==d){
-    				answer = (a+b) * Math.abs(a-b);
-    			}else {
-    				answer = a*d;
-    			}
-    		}else {
-    			if(c==d && a==d) {
-    				answer = (10 * a + b) * (10 * a + b);
-    			}else if(c==d && a!=d){
-    				answer = (a+b) * Math.abs(a-b);
-    			}else if(a==c && c!=d && a!=d){
-    				answer = (b*d);
-    			}else {
-    				answer = min;
-    			}
-    		}
-    	}
-    	
-    	System.out.println(answer);
+    	String my_string = "cvsgiorszzzmrpaqpe";
+    	int[] list = {16,6,5,3,12,14,11,11,17,12,7};
+    	String answer = "";
+    	String[] arr = my_string.split("");
+    	System.out.print(Arrays.toString(arr));
+
     }
 }
