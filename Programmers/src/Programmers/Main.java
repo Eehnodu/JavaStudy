ae99bf0be3d727e1af982270fcf5b175b34bce6a
WooDongHee <WooDongHee@gmail.com>
2024-01-10 17:52:32 +0900

    5

--- Programmers/src/Programmers/Main.java
+++ Programmers/src/Programmers/Main.java
@@ -6,17 +6,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-    	String str = "rermgorpasam";
+    	String str = "rermgorpsam";
     	int[][] queries = {{2,3}, {0,7}, {5,9}, {6,10}};
     	
-    	String[] result = str.split("");
-    	System.out.println(Arrays.toString(result));
+        char[] result = str.toCharArray();
     	
     	for(int i = 0; i<queries.length; i++) {
-    		for(int j = 0; j<2; j++) {
-    			int s = queries[i][j];
-    			int e = queries[i][j];
+    		int s = queries[i][0];
+			int e = queries[i][1];
+    		for(int j = s; j<=(s+e)/2; j++) {
+    			char tmp = result[j];
+    			result[j] = result[s+e-j];
+    			result[s+e-j] = tmp;
     		}
+    		System.out.print(Arrays.toString(result));
+    		System.out.println();
     	}
+    	System.out.print(Arrays.toString(result));;
     }
 }
